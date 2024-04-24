eb41fa7bab36ac417c2c8736b1d9750cd4c28be9
erickdias0 <32880122+erickdias0@users.noreply.github.com>
2024-04-24 16:44:40 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,52 +1,5 @@
-$('#telefone').mask('(00) 00000-0000')
-$('#cpf').mask('000.000.000-00')
-$('#cep').mask('00000-000')
-$('form').validate({
-    rules: {
-    nome:{
-        required: true
+$(document).ready(function(){
+    console.log('carregou')
+    $('#phone').mask('(00) 0 0000-0000')
 
-
-    },
-    email:{
-        required: true,
-        email:true
-    },
-    telefone:{
-        required: true
-    },
-    endereço:{
-        required:true
-    },
-    cep: {
-        required:true
-    }
-    },
-    messages:{
-        nome:'Favor inserir seu nome completo',
-        email:'Favor inserir um email válido',
-        telefone:'Favor inserir seu telefone com DDD',
-        end:'Favor inserir seu endereço completo',
-        cep: 'Favor inserir um CEP válido'
-
-        
-    }
 })
-
-$('form').on('submit', function(e){
-    if ( !$('#end').val().includes(' ') ){
-        e.preventDefault()
-        alert('Endereço Inválido')
-    }
-})
-
-$('form').on('submit', function(e){
-    if ( !$('#nome').val().includes(' ') ){
-        e.preventDefault()
-        alert('Nome Inválido')
-    }
-})
-
-
-
-
